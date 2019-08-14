3aa36474c4b387f125f7ed33970dedcbd6420c2a
JavFr <jfbrustolon@gmail.com>
2019-08-14 10:14:06 -0300

    Updates

--- precache-manifest.52f380e07f87c610cf889cbc20f67b44.js
+++ precache-manifest.52f380e07f87c610cf889cbc20f67b44.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4bd35233c7b183585e96b74557ecbde7",
+    "revision": "3052844186c5930a4766f5610ca35996",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.879ee194.chunk.css"
   },
   {
-    "revision": "56339761fcb37af34f5e",
+    "revision": "fddfd88c60c3c0c5b28c",
     "url": "/static/css/main.7b3ff0bb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.623397fc.chunk.js"
   },
   {
-    "revision": "56339761fcb37af34f5e",
-    "url": "/static/js/main.4b866415.chunk.js"
+    "revision": "fddfd88c60c3c0c5b28c",
+    "url": "/static/js/main.763bf5a6.chunk.js"
   },
   {
     "revision": "57087445b8c3d9b59894",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dd885152a92d8dfcf670f707f0db351.js"
+  "/precache-manifest.52f380e07f87c610cf889cbc20f67b44.js"
 );
 
 self.addEventListener('message', (event) => {
